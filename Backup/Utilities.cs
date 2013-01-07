f9a4391ee3082e3cde3f4a858cf977bdff41bfa4
mark <mark@plantheric.com>
2013-01-07 15:17:22 +0000

    Write database backups to temp folder + zip up file

---
+++ Backup/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopBackup
+{
+    class Utilities
+    {
+        public static string MakeZipFile(string filePath)
+        {
+            string zipFilePath = filePath + ".zip";
+
+            using (ZipArchive archive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+            }
+
+            return zipFilePath;
+        }
+
+        public static string SystemTempFolder { get { return Environment.GetEnvironmentVariable("TEMP", EnvironmentVariableTarget.Machine); } }
+    }
+}
