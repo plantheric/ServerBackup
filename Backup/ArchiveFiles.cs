74731c8f8447d7eca81e6250421178978d0bba36
mark <mark@plantheric.com>
2013-01-07 20:07:52 +0000

    refactor archive file

--- Backup/ArchiveFiles.cs
+++ Backup/ArchiveFiles.cs
@@ -13,40 +13,32 @@ namespace nopBackup
     {
         public List<UploadItem> GetFilesToUpload()
         {
-            string fullPrefix = BaseKeyPrefix + @"/" + SubKeyPrefix + @"/";
-            var filesToUpload = new List<UploadItem>();
+            string fullPrefix = string.Format("{0}/{1}/", BaseKeyPrefix, SubKeyPrefix);
 
             List<string> localFiles = new List<string>(Directory.GetFiles(LocalDirectory));
             localFiles = localFiles.ConvertAll(f => Path.GetFileName(f));
 
-            var request = new ListObjectsRequest
-            {
-                BucketName = AWSBucket,
-                Prefix = fullPrefix,
-                Delimiter = @"/"
-            };
+            var request = new ListObjectsRequest { BucketName = AWSBucket, Prefix = fullPrefix, Delimiter = @"/" };
 
-            List<string> s3Files = new List<string>();
+            List<string> s3Files;
 
             using (var s3Client = AWSClientFactory.CreateAmazonS3Client(AWSAccessKey, AWSSecretKey))
             {
                 using (var response = s3Client.ListObjects(request))
                 {
-                    foreach (S3Object file in response.S3Objects)
-                    {
-                        s3Files.Add(file.Key.Substring(fullPrefix.Length));
-                    }
+                    s3Files = response.S3Objects.ConvertAll(f => f.Key.Substring(fullPrefix.Length));
                 }
             }
 
             localFiles = new List<string>(localFiles.Except(s3Files));
             localFiles = localFiles.ConvertAll(f => Path.Combine(LocalDirectory, f));
 
-            foreach (var file in localFiles)
-            {
-                filesToUpload.Add(new UploadItem { FilePath = file, KeyPrefix = SubKeyPrefix, Lifetime = TimeSpan.MaxValue });
-            }
-
+            var filesToUpload = localFiles.ConvertAll(f => new UploadItem
+                                                            {
+                                                                FilePath = f,
+                                                                KeyPrefix = SubKeyPrefix,
+                                                                Lifetime = TimeSpan.MaxValue
+                                                            });
             return filesToUpload;
         }
 
